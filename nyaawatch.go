3db5b0968f8c0f220ba565dca675a12332e26456
Warren Kenny <warren@ufa.ie>
2015-02-18 14:57:35 +0000

    Fixed bug that prevented refresh from occurring

--- nyaawatch.go
+++ nyaawatch.go
@@ -35,67 +35,69 @@ func downloaded( episode Series ) {
 }
 
 func refresh( config WatchConfig, wg *sync.WaitGroup ) {
-	resp, err := http.Get( config.URL )
+	for {
+		resp, err := http.Get( config.URL )
 
-	if err != nil {
-		fmt.Println( "Could Not Retrieve RSS Feed: ", err )
-		return
-	}
-
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll( resp.Body )
-
-	if err != nil {
-		fmt.Println( "Could Not Read RSS Feed Body: ", err )
-		return
-	}
-
-	rss, err := ReadRSS( body )
-
-	if err != nil {
-		fmt.Println( "Could Not Parse RSS Feed Body: ", err )
-		return
-	}
-
-	for _, e := range rss.Items.ItemList {
-		series, err := ParseSeries( e.Title, config.Regexes )
-
-		if err != nil || alreadyDownloaded( series ) {
-			continue
+		if err != nil {
+			fmt.Println( "Could Not Retrieve RSS Feed: ", err )
+			return
 		}
 
-		for _, s := range config.Series {
-			if strings.EqualFold( s.Name, series.Name ) && strings.EqualFold( s.Subber, series.Subber ) && strings.EqualFold( s.Quality, series.Quality ){
-				path := filepath.Join( config.Directory, s.Subber + "." + s.Name + "." +  strconv.Itoa( series.Episode ) + "." + s.Quality + ".torrent" )
+		defer resp.Body.Close()
 
-				out, err := os.Create( path )
-				defer out.Close()
+		body, err := ioutil.ReadAll( resp.Body )
 
-				if err != nil {
-					fmt.Println( "Failed to create file at ", path, ": ", err )
-					continue
-				}
+		if err != nil {
+			fmt.Println( "Could Not Read RSS Feed Body: ", err )
+			return
+		}
 
-				resp, err := http.Get( e.Link )
-				defer resp.Body.Close()
+		rss, err := ReadRSS( body )
 
-				_, err = io.Copy( out, resp.Body )
+		if err != nil {
+			fmt.Println( "Could Not Parse RSS Feed Body: ", err )
+			return
+		}
 
-				if err != nil {
-					fmt.Println( "Failed to write file at ", path, ": ", err )
-					continue
-				}
+		for _, e := range rss.Items.ItemList {
+			series, err := ParseSeries( e.Title, config.Regexes )
 
-				downloaded( series )
-
-				fmt.Println( "Downloaded Torrent for ", s.Name, " to ", path )
+			if err != nil || alreadyDownloaded( series ) {
+				continue
 			}
 
-		}
-	}
+			for _, s := range config.Series {
+				if strings.EqualFold( s.Name, series.Name ) && strings.EqualFold( s.Subber, series.Subber ) && strings.EqualFold( s.Quality, series.Quality ){
+					path := filepath.Join( config.Directory, s.Subber + "." + s.Name + "." +  strconv.Itoa( series.Episode ) + "." + s.Quality + ".torrent" )
 
-	time.Sleep( time.Second * time.Duration( config.Refresh ) )
+					out, err := os.Create( path )
+					defer out.Close()
+
+					if err != nil {
+						fmt.Println( "Failed to create file at ", path, ": ", err )
+						continue
+					}
+
+					resp, err := http.Get( e.Link )
+					defer resp.Body.Close()
+
+					_, err = io.Copy( out, resp.Body )
+
+					if err != nil {
+						fmt.Println( "Failed to write file at ", path, ": ", err )
+						continue
+					}
+
+					downloaded( series )
+
+					fmt.Println( "Downloaded Torrent for ", s.Name, " to ", path )
+				}
+
+			}
+		}
+
+		time.Sleep( time.Second * time.Duration( config.Refresh ) )
+	}
 }
 
 func main() {
